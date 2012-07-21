60bdeb736e5ad12922ed99787fa4912ce4bf0414
Tobias Oetiker <tobi@oetiker.ch>
2012-07-21 15:16:32 +0200

    added skeletons

---
+++ frontend/source/class/qr/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("qr.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
---
+++ frontend/source/class/qr/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("qr.theme.Theme",
+{
+  meta :
+  {
+    color : qr.theme.Color,
+    decoration : qr.theme.Decoration,
+    font : qr.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : qr.theme.Appearance
+  }
+});
